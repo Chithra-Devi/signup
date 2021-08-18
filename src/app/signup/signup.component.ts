6c6bcf96230a7dd15b4a9ff79bfe8ae3d7b1f917
Chithra-Devi <88471823+Chithra-Devi@users.noreply.github.com>
2021-08-17 20:28:31 -0400

    first commit

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  successMessage: string | undefined;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.register();
+  }
+  register(){
+    console.log("Hello Message");
+    // console.log(this.register)
+    // this.successMessage = "Successfully Registered..."
+  }
+  }
+
